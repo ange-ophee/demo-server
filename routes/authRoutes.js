a5f5d16e3239aeb01fcd7e673a7dace63d75d2e4
Manu <manuange@example.com>
2026-02-16 19:52:32 +0100

    modified authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,7 +9,7 @@ const authController = require('../controllers/authController'); // correct impo
  *     summary: Register a new user
  *     tags: [Auth]
  */
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/auth/register', authController.register);
+router.post('/auth/login', authController.login);
 
 module.exports = router;
