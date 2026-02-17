8a755e7397266b9360c0a46cc3ced1d4f5f92e7f
Manu <manuange@example.com>
2026-02-17 16:17:26 +0100

    studentController updated

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,20 +1,46 @@
 const Request = require('../models/Request');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const submitRequest = async (_req, res, studentId) => {
+const getUserFromToken = (req) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('No token provided');
+  }
+
+  const token = authHeader.split(' ')[1];
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+const submitRequest = async (req, res) => {
   try {
-    const newRequest = await Request.create({ student: studentId });
-    return res.status(201).json({ message: 'Request submitted', request: newRequest });
+    const user = getUserFromToken(req);
+
+    const newRequest = await Request.create({
+      student_id: user.id  // 🔥 required field
+    });
+
+    return res.status(201).json({
+      message: 'Request submitted',
+      request: newRequest
+    });
+
   } catch (err) {
-    return res.status(500).json({ message: err.message });
+    return res.status(401).json({ message: err.message });
   }
 };
 
-const viewRequest = async (_req, res, studentId) => {
+const viewRequest = async (req, res) => {
   try {
-    const requests = await Request.find({ student: studentId });
+    const user = getUserFromToken(req);
+
+    const requests = await Request.getByStudentId(user.id);
+
     return res.json({ requests });
+
   } catch (err) {
-    return res.status(500).json({ message: err.message });
+    return res.status(401).json({ message: err.message });
   }
 };
 
